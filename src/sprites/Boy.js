2a41f511707fe364859750a08ea88f8195a181cc
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-09 18:05:41 -0400

    the ultimate push

--- src/sprites/Boy.js
+++ src/sprites/Boy.js
@@ -8,7 +8,7 @@ export default class extends Phaser.Sprite {
   }
 
   create () {
-    this.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7], 1, true)
+    // this.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7], 1, true)
     // this.animations.add('up', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 10, true)
     // this.animations.add('walk')
 
@@ -18,7 +18,7 @@ export default class extends Phaser.Sprite {
   update () {
     console.log('THIS',this)
     // this.angle += -1
-    this.animations.play('walk', 5, true)
+    // this.animations.play('walk', 5, true)
     // if (this.cursors.left.isDown) {
     //   this.animations.play('left')
     //   console.log('TEST left')
