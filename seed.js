ff2b0e99b268355fede8b5f283b3c6f55e12495d
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-16 10:58:03 -0400

    update to seed file for math questions

--- seed.js
+++ seed.js
@@ -30,9 +30,17 @@ async function seed () {
     Question.create({content: 'If you flip a fair coin 10 times, what is the probability of getting all heads?', option1: '0.5^10', option2: '1^10', option3: '0.25^10', option4: '-1^10'}),
     Question.create({content: 'If you flip a fair coin twice, what is the probability of getting at least 1 head?', option1: '0.75', option2: '0.5', option3: '1.25', option4: '-0.5'}),
     Question.create({content: 'If you will flip a fair die once, what is the probability of getting an odd number?', option1: '0.5', option2: '-0.5', option3: '1.5', option4: '0.25'}),
-    Question.create({content: 'If you roll a pair offair dice once, what is hte probability ofgetting a sum of 5?', option1: '1/9', option2: '1/4', option3: '-1/4', option4: '1.25'}),
+    Question.create({content: 'If you roll a pair of fair dice once, what is the probability of getting a sum of 5?', option1: '1/9', option2: '1/4', option3: '-1/4', option4: '5/4'}),
     Question.create({content: 'If you roll a fair die twice, what is the probability of getting the same number twice?', option1: '1/6', option2: '-1/6', option3: '1/12', option4: '7/6'}),
     Question.create({content: 'If you roll a fair die twice, what is the probability of getting 2 different numbers?', option1: '5/6', option2: '1/6', option3: '-5/6', option4: '7/6'})
+  ])
+
+  const basicGeometry = await Promise.all([
+    Question.create({content: 'Suppose you have a scoop of ice cream that is a perfect sphere and the scoop has a radius of 3 inches. What is the volume?', option1: '4/3πr^3', option2: '3/4πr^3', option3: '4/3πr^2', option4: '3/4πr^2' }),
+    Question.create({content: 'Suppose you have a cone to put your ice cream in. How can you determine the surface area for your cone?', option1: '2πr', option2: '3πr', option3: '2πr^2', option4: '-3πr^2' }),
+    Question.create({content: 'What is the area of the roof of the bakery? It has a height of 8 feet and a base of 20 feet? What is the area?', option1: '80 feet', option2: '40 feet^2', option3: '-80 feet^2', option4: '100 feet^2' }),
+    Question.create({content: 'Suppose the bakery has a width of 20 feet and a length of 15 feet. What is the area of the bakery?', option1: '300 feet^2', option2: '-300 feet^2', option3: '400 feet^2', option4: '1500 feet^2' }),
+    Question.create({content: 'Suppose you buy a cookie from the bakery. How do you determine its circumference?', option1: 'πd', option2: 'πr^2', option3: '2π', option4: '2πd' })
   ])
 
   const categories = await Promise.all([
@@ -42,14 +50,20 @@ async function seed () {
     Category.create({type: 'basicProbability'})
   ])
   console.log(`seeded ${basicArithmetic.length} basic arithmetic questions`)
-  console.log(`seeded ${advancedArithmetic.length} advanced arithmetic questions`)
+  console.log(`seeded ${advancedArithmetic.length} advanced arithmetic
+  questions`)
+  console.log(`seeded ${basicGeometry.length} basic geometry questions`)
   console.log(`seeded ${basicProbability.length} basic probability questions`)
   console.log(`seeded ${categories.length} categories`)
   for (let i = 0; i < basicArithmetic.length; i++) {
-    await basicArithmetic[i].setCategory(categories[0])
-    await advancedArithmetic[i].setCategory(categories[1])
-    await basicProbability[i].setCategory(categories[3])
+      await Promise.all([
+        basicArithmetic[i].setCategory(categories[0]),
+        advancedArithmetic[i].setCategory(categories[1]),
+        basicGeometry[i].setCategory(categories[2]),
+        basicProbability[i].setCategory(categories[3])
+      ])
   }
+  await basicProbability[5].setCategory(categories[3])
 
   const users = await Promise.all([
     User.create({
