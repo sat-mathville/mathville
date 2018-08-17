817a31ff01574d1bfede00104b6d5530a381573b
Bronwyn Harris <bronwyn.harris@Bronwyns-MacBook-Air.local>
2018-08-17 16:38:55 -0400

    commiting before switching to master

--- src/states/helperFunctions/renderFinalOutputMsg.js
+++ src/states/helperFunctions/renderFinalOutputMsg.js
@@ -3,12 +3,11 @@ import wrap from './wrap'
 
 export default function renderFinalOutput (score,questionsLength,id) {
   const str = '\n\nClick HERE to leave'
+  const abilityName = store.getState().abilities.find(ability => ability.id===id)
   if (score === questionsLength) {
     if (store.getState().userAbilities.has(id)) return wrap('Good job! But you have done this before. Go somewhere else!', 50) + str
-    const abilityName = store.getState().abilities.find(ability => ability.id===id)
     return wrap(`Congratulations! You have leveled up! You just got ${abilityName.name}`, 50) + str
   } else {
-    const abilityName = store.getState().abilities.find(ability => ability.id===id)
     return wrap(`I'm sorry. You have not answered enough questions correctly. Please come back to get ${abilityName.name}.`, 50) + str
   }
 }
