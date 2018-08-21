142b833a26023da2ba0dd79f47980a4cbda93e2b
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-21 14:01:16 -0400

    Remove old code in StartGame and update login component for logout and no longer call thunks

--- src/states/StartGame.js
+++ src/states/StartGame.js
@@ -3,47 +3,24 @@ import Phaser from 'phaser'
 import store, {setCoord, fetchAllAbilities, getProblems} from '../store'
 
 export default class extends Phaser.State {
- preload() {
-  this.load.image('playBtn', '../assets/images/playBtn.png')
-  store.dispatch(setCoord([450, 500]))
-  store.dispatch(fetchAllAbilities())
-  store.dispatch(getProblems())
- }
-
- update() {
-   if(!store.getState().user.id){
-    this.renderPleaseLogin()
+  preload () {
+    this.load.image('playBtn', '../assets/images/playBtn.png')
+    store.dispatch(setCoord([450, 500]))
+    store.dispatch(fetchAllAbilities())
+    store.dispatch(getProblems())
   }
-   else if(store.getState().user.id){
+  create () {
     this.renderPlayButton()
-   }
- }
-
- renderPlayButton() {
-  if(this.pleaseLogin){
-    this.pleaseLogin.destroy()
   }
-  if(!this.button1){
-    this.button1 = this.add.button(this.world.centerX,this.world.centerY, 'playBtn', this.actionOnClick, this, 2, 1, 0);
-    this.button1.anchor.setTo(0.5,0.5)
+  renderPlayButton () {
+    this.button1 = this.add.button(this.world.centerX, this.world.centerY, 'playBtn', this.actionOnClick, this, 2, 1, 0)
+    this.button1.anchor.setTo(0.5, 0.5)
 
-    this.txt = this.add.text (this.button1.x, this.button1.y, "Start",  {font:"80px Cinzel", fill:"#fff", align:"center"})
+    this.txt = this.add.text(this.button1.x, this.button1.y, 'Start', {font: '80px Cinzel', fill: '#fff', align: 'center'})
 
-    this.txt.anchor.setTo(0.5,0.5)
+    this.txt.anchor.setTo(0.5, 0.5)
   }
-
- }
- renderPleaseLogin(){
-  if(this.button1){
-    this.button1.destroy()
-    this.txt.destroy()
+  actionOnClick () {
+    this.game.state.start('Game')
   }
-  if(!this.pleaseLogin){
-    this.pleaseLogin = this.add.text(this.world.centerX,this.world.centerY,'Please Login')
-  }
- }
- actionOnClick () {
-  this.game.state.start('Game')
- }
-
 }
