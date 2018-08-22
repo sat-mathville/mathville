52b0d45c2fce06f8f60dbcb41c0ea04424f4185b
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-21 20:36:00 -0400

    Update to move chicken in a rectangle

--- src/states/helperFunctions/chickenMovement.js
+++ src/states/helperFunctions/chickenMovement.js
@@ -1,32 +1,20 @@
 export default (chicken) => {
   setInterval(() => {
-    if (chicken.x === 700 && chicken.y > 420) {
-      chicken.y -= 3
-    }
-    if (chicken.x < 990 && chicken.y === 420) {
-      chicken.x += 3
-    }
-    if (chicken.x === 990 && chicken.y < 600) {
-      chicken.y += 3
-    }
-    if (chicken.x > 700 && chicken.y === 600) {
-      chicken.x -= 3
-    }
-    if (chicken.x === 700 && chicken.y === 600) {
+    if (chicken.y >= 540 && chicken.x <= 700) {
       console.log('1st if statement')
+      chicken.y-=3
       chicken.animations.play('walkUp', 20, true)
-    }
-    if (chicken.x === 700 && chicken.y === 420) {
+    } else if (chicken.x <= 910 && (chicken.y <= 540)) {
       console.log('2nd if statement')
+      chicken.x+=3
       chicken.animations.play('walkRight', 20, true)
-    }
-    if (chicken.x === 990 && chicken.y === 420) {
+    } else if ((chicken.x >= 910) && chicken.y <= 600) {
+      chicken.y +=3
       chicken.animations.play('walkDown', 20, true)
-    }
-    if (chicken.x === 990 && chicken.y===600) {
+    } else if (chicken.x >= 700 && (chicken.y >= 600 && chicken.y <= 610)) {
       console.log('4th if statement')
+      chicken.x-=3
       chicken.animations.play('walkLeft', 20, true)
     }
-
   }, 50)
 }
