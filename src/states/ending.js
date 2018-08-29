617908f257de0739299021b6ff7b951e228d45f4
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-08-29 11:37:25 -0400

    Include exit button from ending scene

--- src/states/ending.js
+++ src/states/ending.js
@@ -1,5 +1,7 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
+import store, {auth} from '../store'
+
 
 export default class extends Phaser.State {
   preload () {
@@ -23,5 +25,20 @@ With ❤
         fill: '#FFFFFF',
         align: 'center'
       })
+
+    // Logout Button
+    this.logoutBtn = this.game.add.button(0, 80, 'logoutBtn', this.actionOnLogout, this)
+    this.logoutBtn.width = 130
+    this.logoutBtn.height = 50
+    this.logoutBtn.x = this.txt.x + 175
+    this.logoutBtn.y = this.txt.y + 400
+    this.exitTxt = this.add.text(this.logoutBtn.x+25, this.logoutBtn.y, 'Exit', {font: '40px Cinzel', fill: '#fff', align: 'center'})
+  }
+  actionOnLogout () {
+    store.dispatch(auth({}, 'logout'))
   }
 }
+
+
+
+
